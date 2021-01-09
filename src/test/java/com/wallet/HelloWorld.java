b7f1da30139552943b1a7ad3ee72b570cb0319cb
Fábio Penteado <flzpenteado@Fabios-MacBook-Pro-2.local>
2021-01-09 17:33:50 -0300

    chore: configure tests

---
+++ src/test/java/com/wallet/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.wallet;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.junit.Assert.assertEquals;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+public class HelloWorld {
+
+    @Test
+    public void testHelloWorld() {
+        assertEquals(1,1);
+    }
+}
